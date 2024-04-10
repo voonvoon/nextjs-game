2f5613cafc97dc52dc89b923e249e9e79c32729c
voon220 <peter220@gmail.com>
2024-04-10 16:38:52 +0800

    21th

--- src/app/api/webhook/payment/route.ts
+++ src/app/api/webhook/payment/route.ts
@@ -1,58 +1,63 @@
-import { NextResponse } from "next/server";
-//import { createOrder, updateGameQuantity } from "@/libs/apis";
+// pages/api/webhook/payment.js
+import { createOrder } from "@/libs/apis";
 const Chip = require("Chip").default;
 
-//Chip set up
+// Chip set up
 Chip.ApiClient.instance.basePath = process.env.ENDPOINT;
 Chip.ApiClient.instance.token = process.env.API_KEY;
 const apiInstance = new Chip.PaymentApi();
 
-const test = [{
-  _id: "testid",
-  name: "testName",
-  images: [],
-  quantity: 121,
-  maxQuantity: 121,
-  price: 121,
-}];
+const test = [
+  {
+    _id: "testid",
+    name: "testNamehaha",
+    images: [],
+    quantity: 121,
+    maxQuantity: 121,
+    price: 121,
+  },
+];
 
-export async function POST(req:any, res: any) {
-  try {
-    const { headers } = req;
-    
-    // Read the request body stream and parse it as JSON
-    let rawBody = '';
-    req.on('data',  (chunk: Buffer) => {
-      rawBody += chunk;
+// Middleware function to handle raw body..
+function rawBodyMiddleware(req: any, res: any, next: any) {
+  let rawBody = "";
+  req.setEncoding("utf8");
+
+  req.on("data", function (chunk: any) {
+    if (chunk) rawBody += chunk;
+  });
+
+  req.on("end", function () {
+    req.rawBody = rawBody;
+    next();
+  });
+}
+
+export async function POST(req: any, res: any) {
+  //nextjs convention , handle incoming req
+  if (req.method === "POST") {
+    rawBodyMiddleware(req, res, async function () {
+      const { rawBody, headers } = req;
+      const parsed = JSON.parse(rawBody);
+      const xsignature = headers["x-signature"];
+      const publicKey = process.env.WEBHOOK_PUBLIC_KEY;
+
+      const verified = apiInstance.verify(
+        rawBody,
+        Buffer.from(xsignature, "base64"),
+        publicKey
+      );
+      console.log("/webhook/payment EVENT===========>: ", parsed.event_type);
+      console.log("/webhook/payment VERIFIED=============>: ", verified);
+
+      // Create order
+      createOrder(test, "test@gmail.com");
+
+      // You can add logic to update db qty here
+
+      res.end("WEBHOOK OK!");
     });
-    req.on('end', () => {
-      try {
-        const parsed = JSON.parse(rawBody);
-        const xsignature = headers["x-signature"];
-        const publicKey = process.env.WEBHOOK_PUBLIC_KEY;
-
-        const verified = apiInstance.verify(
-          rawBody,
-          Buffer.from(xsignature, "base64"),
-          publicKey
-        );
-
-        console.log("/webhook/payment EVENT===========>: ", parsed.event_type);
-        console.log("/webhook/payment VERIFIED=============>: ", verified);
-
-        // Create order
-        //createOrder(test, 'test@gmail.com');
-
-        // Update db qty
-
-        res.end("WEBHOOK OK Voon!");
-      } catch (error) {
-        console.error("Error parsing JSON:", error);
-        res.status(400).end("Bad Request");
-      }
-    });
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).end("Internal Server Error");
+  } else {
+    res.status(405).end(); // Method Not Allowed
   }
 }
