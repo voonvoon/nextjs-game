753e85df546bcf9e32206db24126b8dd5db87dae
voon220 <peter220@gmail.com>
2024-04-13 13:39:56 +0800

    60th

--- src/app/api/webhook/payment/route.ts
+++ src/app/api/webhook/payment/route.ts
@@ -1,68 +1,31 @@
-//import { NextRequest } from "next/server";
-//import { headers } from "next/headers";
-
-import type { Readable } from "node:stream";
-import type { NextApiRequest, NextApiResponse } from "next";
 const Chip = require("Chip").default;
-// Get raw body as string
-
 
 //Chip set up
 Chip.ApiClient.instance.basePath = process.env.ENDPOINT;
 Chip.ApiClient.instance.token = process.env.API_KEY;
 const apiInstance = new Chip.PaymentApi();
 
-// async function getRawBody(readable: Readable): Promise<Buffer> {
-//   const chunks = [];
-//   for await (const chunk of readable) {
-//     chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
-//   }
-//   return Buffer.concat(chunks);
-// }
-
 export async function POST(request: Request, response: Response) {
   try {
-    const rawBody = await request.text()
-    //const rawBody = await getRawBody(request as any);
-    //const {rawBody} = request as any;
-    //const { headers } = request;
-    //const parsed = JSON.parse(rawBody);
+    const rawBody = await request.text();
     const parsed = JSON.parse(rawBody);
-
-    //const xsignature = headers["x-signature"];
-    //const xsignature = (headers as any)["x-signature"];
     const xsignature = request.headers.get("x-signature");
-    //const xSignature = request.headers['x-signature'];
-    
-
 
     const publicKey = process.env.WEBHOOK_PUBLIC_KEY;
-
-    // const verified = apiInstance.verify(
-    //   rawBody,
-    //   Buffer.from(xsignature as any, "base64"),
-    //   publicKey
-    // );
-
-    // console.log("/webhook/payment EVENT: ", parsed.event_type);
-    // console.log("/webhook/payment VERIFIED: ", verified);
     if (xsignature !== null) {
       const verified = apiInstance.verify(
         rawBody,
         Buffer.from(xsignature, "base64"),
         publicKey
       );
-      console.log("/webhook/payment EVENT: ", parsed.event_type);
-      console.log("/webhook/payment VERIFIED: ", verified);
-      console.log("i want see how signature looks like ===>",  xsignature);
+      console.log("/webhook/payment EVENT ===> ", parsed.event_type);
+      console.log("/webhook/payment VERIFIED ===> ", verified);
+      console.log("i want see how signature looks like ===>", xsignature);
       console.log("love to see what inside parse ===>", parsed);
       console.log("love to see what inside rawBody ===>", rawBody);
-      
-
     } else {
       console.log("X-Signature header is null");
     }
-    
 
     // Process the webhook payload
   } catch (error) {
@@ -75,8 +38,3 @@ export async function POST(request: Request, response: Response) {
     status: 200,
   });
 }
-
-
-
-
-
