6812a90c4dc69720cccbc8abd744adc6df27d4fa
voon220 <peter220@gmail.com>
2024-04-23 11:59:56 +0800

    105th

--- src/components/GameCard/gameCardClassNames.ts
+++ src/components/GameCard/gameCardClassNames.ts
@@ -1,10 +1,10 @@
 const gameCardClassNames = {
-    container: "relative mx-auto w-[251px] h-[277px] rounded-2xl overflow-hidden",
-    image: "w-full h-full object-cover",
+    container: "relative mx-auto w-[251px] h-[277px] rounded-2xl overflow-hidden transition duration-300 transform focus:scale-110 hover:shadow-lg hover:border-white-500",
+    image: "w-full h-full object-cover hover:scale-110 transition-all duration-150 ",
     price:
-      "absolute rounded-bl-2xl top-0 right-0 bg-primary p-3 text-lg font-semibold z-10",
+      "absolute rounded-bl-2xl top-0 right-0 bg-primary p-3 text-lg font-semibold z-10 ",
     gameName:
-      "absolute bottom-0 left-0 right-0 py-3 px-4 text-white font-bold text-lg bg-gray-900 bg-opacity-75",
+      "absolute bottom-0 left-0 right-0 py-3 px-4 text-white font-bold text-lg bg-gray-900 bg-opacity-75  transition duration-300 transform hover:scale-110",
   };
   
   export default gameCardClassNames;
