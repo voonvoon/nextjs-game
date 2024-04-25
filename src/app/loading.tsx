02ddce15e4303035f91d36446a6190fab3160431
voon220 <peter220@gmail.com>
2024-04-25 13:53:10 +0800

    113th

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,9 @@
 export default function Loading() {
-  return (
-    <div className="flex justify-center items-center h-full">
-      Loading...
-      <div className="animate-spin rounded-full h-6 w-6 border-t-2 border-b-2 border-gray-200"></div>
-    </div>
-  );
-}
+    return (
+      <div className="flex justify-center items-center h-full text-white">
+        Loading...
+        <div className="animate-spin rounded-full h-6 w-6 border-t-2 border-b-2 border-white"></div>
+      </div>
+    );
+  }
+  
