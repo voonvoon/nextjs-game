dcc123cb5965254c4bed54b1f28f1791d6242750
voon220 <peter220@gmail.com>
2024-04-25 11:44:23 +0800

    107th

--- src/libs/emailService.ts
+++ src/libs/emailService.ts
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 const Mailgen = require("mailgen");
-require("dotenv").config();
+
 
 const emailSubPaid = async (emailToSend:string) => {
   let transporter = nodemailer.createTransport({
